25d4d2fe9e02e6fbeb944b45d3db8e72ee39e290
silkslime <silkslime@yandex.ru>
2021-12-16 04:53:48 +0300

    WIP full story but no quest))

--- other/chuiko/js/script.js
+++ other/chuiko/js/script.js
@@ -1,10 +1,16 @@
 const debug = false;
-function log(txt, color="rgb(196, 255, 186)", time=0) {
+function log(txt, color = "rgb(196, 255, 186)", time = 0, type = false) {
     if (debug) time = 0;
     let t = document.createElement('h1');
-        t.style.setProperty('color', color, 'important');
-    setTimeout(function() {
+    t.style.setProperty('color', color, 'important');
+    setTimeout(function () {
         terminal.appendChild(t);
+        if (type == 2) {
+            t.classList.add("typing");
+        }
+        if (type == 1) {
+            t.classList.add("typingfast");
+        }
         t.innerHTML = txt;
         window.scrollBy(0, 1000);
     }, time);
@@ -19,4 +25,11 @@ function cmd(prefix, command, listener, time) {
     lsla.innerHTML = command;
     lsla.addEventListener("click", listener)
     setTimeout(function () { newcmd.appendChild(lsla); }, time);
+}
+
+function typer(elem, txt, speed) {
+    if (elem.innerHTML.length != txt.length) {
+        elem.innerHTML += txt.charAt(elem.innerHTML.length);
+        setTimeout(function () { typer(elem, txt) }, speed)
+    }
 }
